09f8e780d4e4e771ed778a61c58c276b28461d31
Rafael Augusto da Silva <rafaelaugustodasilva2@gmail.com>
2024-10-09 23:28:34 -0300

    Create User Route

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import userRouter from "./routes/user.routes.js"
 import authRouter from "./routes/auth.routes.js"
 
 const app = express()
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Routes
+app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 
 app.listen(3000, () => {
