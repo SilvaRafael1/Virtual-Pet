229dfb028d1abe82143c5a03706f7afc92edc6a7
Rafael Augusto da Silva <rafaelaugustodasilva2@gmail.com>
2024-10-09 23:27:43 -0300

    Create user.controller.js

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,45 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+export default {
+  async getUserById(req, res) {
+    try {
+      const id = parseInt(req.params.id)
+      const user = await prisma.user.findUnique({
+        where: {
+          id
+        }
+      })
+
+      if (!user) 
+        return res.json("User not found")
+      
+      return res.json({ user: user })
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
+  async updateUser(req, res) {
+    try {
+      const id = parseInt(req.params.id)
+      const { data } = req.body
+      const updatedUser = await prisma.user.update({
+        where: {
+          id
+        },
+        data: {
+          data
+        }
+      })
+
+      if (!updatedUser) 
+        return res.json("User not found")
+      
+      return res.json({ user: updatedUser })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
